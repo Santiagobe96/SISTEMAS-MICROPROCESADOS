f95949513559e19c0d9d9116cf5e110d1b8055ef
Santiago Bolaños <49934473+Santiagobe96@users.noreply.github.com>
2019-05-19 18:01:45 -0500

    Add files via upload

---
+++ Deber_3_Cedula.ino
@@ -0,0 +1,90 @@
+/*
+ * UNIVERSIDAD TÉCNICA DEL NORTE
+   FICA - CIERCOM
+   NOMBRE: SANTIAGO BOLAÑOS
+   CURSO: 6to "A"
+   FECHA: 19/05/2019
+   TEMA: Valide la cédula ecuatoriana que se ingresa por comunicación serial con el Formato. 100124578-9 
+  */
+String cedula;
+String dato1;
+String Cadena1;
+String Cadena2;
+int TamCedula = 10;
+int NumProvin = 24;
+int TercerDigito = 6;
+int Vector [] = {2, 1, 2, 1, 2, 1, 2, 1, 2};  //coeficientes para realizar la verificaciòn
+int dato2 = 0;
+int total = 0;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Ingrese su numero de cedula en el formato 012345678-9");
+  Serial.println(" ");
+}
+
+void loop() {
+   
+  if(Serial.available()>0 ){
+  dato1=Serial.readString();
+  Serial.println(" ");
+  Cadena1=dato1.substring(0,dato1.indexOf("-"));  
+  Cadena2=dato1.substring(dato1.indexOf("-")+1,dato1.length()); 
+  cedula=(Cadena1+Cadena2);
+
+  if (cedula.length() == TamCedula) {  // si la longitud de la cedula es igual al tamaño 10 sigue el proceso        
+    int Provin = (cedula.substring(0,2)).toInt(); 
+    int DigTres = (cedula.substring(2,3)).toInt();  
+               
+    if ((Provin > 0 && Provin <= NumProvin) && DigTres < TercerDigito) {  
+      int DigVeriRecibido = (cedula.substring(9,10)).toInt();
+        for (int i = 0; i < 9; i++) {
+          int val = Vector[i] * (cedula.substring(i,i+1)).toInt();
+          total = val >= 10 ? total + (val - 9) : total + val;
+        }
+
+      int DigVeriObtenido = (total % 10) > 0 ? 10 - (total % 10) : (total % 10);
+        if (DigVeriObtenido == DigVeriRecibido) {
+          Serial.print("El numero de cedula: ");
+          Serial.print(cedula);
+          Serial.println(" es CORRECTO");
+          Serial.println(" ");
+          delay(200);
+          dato2 = -1;
+        } 
+        else {
+          Serial.print("El numero de cedula: ");
+          Serial.print(cedula);
+          Serial.println(" es INCORRECTO");
+          
+          delay(200);
+        }  
+    } 
+    else{
+          Serial.print("El numero de cedula: ");
+          Serial.print(cedula);
+          Serial.println(" es INCORRECTO"); 
+          Serial.println(" ");
+          delay(200);
+    }
+  } 
+  else if (cedula.length() < TamCedula) { //si la longitud de la cedula es menor al tamaño 10 envia un mensaje para volver a escribir la cedula
+    Serial.println("");
+    Serial.print("El numero de cedula: "); 
+    Serial.print(cedula);
+    Serial.println(" tiene menos de 10 digitos");
+    Serial.println(" ");
+    delay(200);
+
+  }
+  else if (cedula.length() > TamCedula) { //si la longitud de la cedula es mayor al tamaño 10 envia un mensaje para volver a escribir la cedula
+    Serial.println("");    
+    Serial.print("El numero de cedula: "); 
+    Serial.print(cedula);
+    Serial.println(" tiene mas de 10 digitos");
+    Serial.println(" ");
+    delay(200);
+  } 
+  }
+}
+     
